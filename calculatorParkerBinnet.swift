d27b254459a0591142da29d2735d21e58962d9de
Parker <94702972+park-err@users.noreply.github.com>
2023-03-04 18:44:32 -0600

    committing calc playground

---
+++ calculatorParkerBinnet.swift
@@ -0,0 +1,35 @@
+// vars
+let one = 1
+let two = 2
+let three = 3
+let four = 4
+let five = 5 ,
+let six = 6
+let seven = 7
+let eight = 8
+let nine = 9
+var op1 = 0
+var op2 = 0
+var op3 = 0
+var op4 = 0
+var op5 = 0
+
+// operations
+op1 = eight / two
+op2 = seven * four
+op3 = five - eight
+op4 = nine + nine
+op5 = three % two
+
+// print to console
+print("Operations")
+print("8 / 2")
+print(op1)
+print("7 * 4")
+print(op2)
+print("5 - 8")
+print(op3)
+print("9 - 9")
+print(op4)
+print("3 % 2")
+print(op5)
