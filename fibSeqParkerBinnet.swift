8122bda3b14b52774a867492f8eb28e085b958b3
Parker <94702972+park-err@users.noreply.github.com>
2023-03-07 10:52:12 -0600

    committing fibonacci sequence function

---
+++ fibSeqParkerBinnet.swift
@@ -0,0 +1,20 @@
+import UIKit
+func fibSeq(previousNum: Int, numBeforePrev: Int) -> Int {
+    return previousNum + numBeforePrev
+}
+
+func main() {
+    var seq = [0, 1]
+    let LENGTH = 20
+    var i = 2
+    
+    while (i < LENGTH) {
+        seq.append(fibSeq(previousNum: seq[i-1], numBeforePrev: seq[i-2]))
+        i += 1
+    }
+    
+    print(seq)
+}
+
+
+main()
