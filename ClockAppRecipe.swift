dd9cd713015223f2a820c20891c733a541bb26bb
Parker <94702972+park-err@users.noreply.github.com>
2023-05-11 11:05:51 -0500

    commit ClockAppRecipe.swift

---
+++ ClockAppRecipe.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Clock")
+            HStack {
+                Text("hour")
+                Text("minute")
+                Text("second")
+            }
+        }
+    }
+}
